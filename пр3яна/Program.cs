790cf61716d8cdc00e7f85f44a7a2308a96b4d49
Студент <@65-10>
2026-09-24 14:46:53 +0300

    Добавьте файлы проекта.

---
+++ пр3яна/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace пр3яна
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
